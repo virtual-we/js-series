bf4db6ea14ade6f879f732f7007ebfa00a1e6102
Aman Kumar <akdbg65@gmail.com>
2024-08-12 10:39:02 +0000

    for in concept

--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,9 @@
-console.log('Aman')
+const a={
+    Name: "Aman",
+    IsLogIn:true
+}
+for(const {key, value} of a){
+        console.log(key,':-', value);
+}
+//  for of is for array and map but not for object
+// for in is for object 
